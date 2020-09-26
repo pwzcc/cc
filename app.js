5c812dfd5b9a81ed984db52f67501c01d593395d
pwzcc <panthersuper@hotmail.com>
2020-09-26 11:45:57 -0400

    api back

--- app.js
+++ app.js
@@ -34,27 +34,24 @@ new Vue({
                 })
             }
 
-
-            DB.getData("warlogs", (wars) => {
-                this.wars = wars
-                this.loadingComplet = true
-    
-                if (!this.currentWarData){
-                    let times = Object.keys(this.wars)
-    
-                    this.currentWarData = this.wars[times[times.length-1]]
-                    this.startingWarData = this.recentWars[0]
-    
-                }
-    
-                console.log('this.wars', this.wars)
-    
-                console.log('recentWarMemberInfo', this.recentWarMemberInfo())
-            })
-    
-
         })
 
+        DB.getData("warlogs", (wars) => {
+            this.wars = wars
+            this.loadingComplet = true
+
+            if (!this.currentWarData){
+                let times = Object.keys(this.wars)
+
+                this.currentWarData = this.wars[times[times.length-1]]
+                this.startingWarData = this.recentWars[0]
+
+            }
+
+            console.log('this.wars', this.wars)
+
+            console.log('recentWarMemberInfo', this.recentWarMemberInfo())
+        })
 
         
 
